36d4f7f36e1b41e7d4019177aaaf7e7955e528ec
Roberto <roberto.scardia@gmail.com>
2025-01-01 20:14:28 +0100

    fixed ball script but still physics broke

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -4,13 +4,20 @@ extends RigidBody3D
 
 var input_dir: Vector3 = Vector3(0,0,0)
 var input_vel: float = 0
-var input_rec: bool = false
+var input_imp: bool = false
 
 func _ready():
-	transform.origin = Vector3(0,0.5,0)
+	transform.origin = Vector3(0,1,0)
 	player.ball_moved.connect(apply_input)
 	
 func apply_input(direction: Vector2, velocity: float) -> void:
 	input_dir = Vector3(direction.x, 0, direction.y).normalized()
 	input_vel = velocity
-	self.apply_impulse(input_dir*input_vel)
+	self.input_imp = true
+	
+
+func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
+	
+	if self.input_imp:
+		state.set_linear_velocity(state.get_linear_velocity() + input_dir*input_vel)
+		self.input_imp = false
